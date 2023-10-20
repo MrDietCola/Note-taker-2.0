ddd20fb89d35d5363dc79c0e5108c1e3b8bcc8dc
AJ Seidler <ajseidler0526@gmail.com>
2023-10-20 13:26:44 -0500

    added code from original project and added module exports in notes.js and added functionality for delete method

---
+++ routes/notes.js
@@ -0,0 +1,56 @@
+const notes = require('express').Router();
+const { readAndAppend, readFromFile, writeToFile } = require('../helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
+
+notes.get('/', (req, res) => {
+  console.info(`${req.method} request received for notes`);
+
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+notes.post('/', (req, res) => {
+  console.info(`${req.method} request received to submit notes`);
+
+  const note = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title: note.title,
+      text: note.text,
+      id: uuidv4()
+    }
+
+    console.log(note);
+
+    readAndAppend(newNote, './db/db.json');
+    
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting feedback');
+  }
+});
+
+notes.delete('/:id', (req, res) => {
+  console.info(`${req.method} request received to delete a note`);
+
+  const noteToDelete = req.params.id;
+  
+  readFromFile('./db/db.json').then((data) => {
+    const notesData = JSON.parse(data);
+    for (let i = 0; i < notesData.length; i++) {
+      const noteId = notesData[i].id;
+      if (noteId === noteToDelete) {
+        notesData.splice(i, 1)
+      }
+    }
+
+    writeToFile('./db/db.json', notesData)
+  });
+});
+
+module.exports = notes;
